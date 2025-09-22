6c8197f83584771fd695a57e2256b6da2f7af54c
Gabriel Pires <gabrielhpires@gmail.com>
2025-09-21 22:22:21 -0300

    feat: Added RSS/Atom feed to the blog

---
+++ blog/feeds.py
@@ -0,0 +1,24 @@
+import markdown as md
+from django.contrib.syndication.views import Feed
+from django.template.defaultfilters import truncatewords_html
+from django.urls import reverse_lazy
+
+from .models import Post
+
+
+class LatestPostsFeed(Feed):
+    title = "My blog"
+    link = reverse_lazy("blog:post_list")
+    description = "New posts of my blog."
+
+    def items(self):
+        return Post.published.all()[:5]
+
+    def item_title(self, item: Post):
+        return item.title
+
+    def item_description(self, item: Post):
+        return truncatewords_html(md.markdown(item.body), 30)
+
+    def item_pubdate(self, item: Post):
+        return item.publish
